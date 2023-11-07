b8cb7f874157af4a6b1de27c330530d93336579d
Selvarumugam <arumugamselva60@gmail.com>
2023-11-07 10:17:17 +0530

    first commit

---
+++ src/ListElements.jsx
@@ -0,0 +1,17 @@
+function ListElements()
+{
+    return(
+        <ul>
+            <li>hello</li>
+            <li>world</li>
+        </ul>
+    );
+}
+function Add()
+{
+    return(
+        <p>123</p>
+    );
+}
+
+export {ListElements,Add};
